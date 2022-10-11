95c410c662c79c0ee58e614485591fac5d40ba81
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-10-11 19:15:41 -0300

    :sparkles: feat: appBar and image added

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,24 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text('Olá mundo!'),
+    MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        backgroundColor: Colors.blueGrey[900],
+        title: const Text(
+          "Eu sou Rico",
+        ),
       ),
-    ),
+      body: const Center(
+        child: Center(
+          child: Image(
+            image: NetworkImage(
+              'https://images.unsplash.com/photo-1561414927-6d86591d0c4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW9uZXl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            ),
+          ),
+        ),
+      ),
+    )),
   );
 }
