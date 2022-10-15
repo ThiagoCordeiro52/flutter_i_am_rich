dd4e42b44c6fcb3eb364f697cfed2a60f370b71e
Thiago de Oliveira Cordeiro <thiagoty52@gmail.com>
2022-10-15 12:04:01 -0300

    :sparkles: feat: added rubi image

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,8 @@ void main() {
       body: const Center(
         child: Center(
           child: Image(
-            image: NetworkImage(
-              'https://images.unsplash.com/photo-1561414927-6d86591d0c4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW9uZXl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
+            image: AssetImage(
+              'images/rubi.png',
             ),
           ),
         ),
